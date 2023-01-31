7fc7275613f9073946eef4475c03529180576319
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2023-01-31 17:16:02 +0100

    progress day 1

--- code/script.js
+++ code/script.js
@@ -1,8 +1,19 @@
 // Selecting an HTML element
-const elementToToggle = document.querySelector("#section1");
+// const elementToToggle = document.querySelector("#section1");
 // A function that adds and remove the class "active" 
-const toggleHTMLElementClass = () => {
-  elementToToggle.classList.toggle("active");
-};
+// const toggleHTMLElementClass = () => {
+//  elementToToggle.classList.toggle("active");
+//};
 // Adding the toggle function to our selected element, so everytime it's clicked the class will be toggled
-elementToToggle.onclick = toggleHTMLElementClass;
+//elementToToggle.onclick = toggleHTMLElementClass;
+
+section1.onclick = toggleAnswer
+function toggleAnswer() {
+ this.classList.toggle('active') 
+}
+
+document.getElementById("section1").onclick = function () {toggleAnswer() };
+
+function toggleAnswer() { 
+  section1.classList.toggle('open');
+}
