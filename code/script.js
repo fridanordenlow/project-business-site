ecbda2e49541e043f64ee5a332d05a1c6ca1dee5
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2023-02-01 17:01:54 +0100

    cleaned up code

--- code/script.js
+++ code/script.js
@@ -10,10 +10,6 @@ const toggleHTMLElementClass2 = () => {
     section2.classList.toggle("active");
   }
     section2.onclick = toggleHTMLElementClass2;
-    
-
-
-
 
 
 // Selecting an HTML element
@@ -24,18 +20,3 @@ const toggleHTMLElementClass2 = () => {
 //};
 // Adding the toggle function to our selected element, so everytime it's clicked the class will be toggled
 //elementToToggle.onclick = toggleHTMLElementClass;
-
-
-
-//Tried something /Frida
-
-//Selected the element "Section 1" and gave it a shortened name "sect1":
-//const sect1 = document.getElementById("section1")
-
-//This is a function that adds and removes the class "active" (we need to add css to make it behave "actively")
-//const toggleHTMLElementClass = () => {
-//sect1.classList.toggle('active') 
-//}
-
-//Defines when the toggle function "happens" (when we click on "sect1" the classList should become "active")
-//sect1.onclick = toggleHTMLElementClass;
